f516a71f21db3173e61774374698a01f7d3d0953
Amar` <Amar44chauhan@gmail.com>
2026-06-16 00:30:16 +0530

    Add Google tag (gtag.js) analytics script immediately after opening head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C4MJ8ENML7" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-C4MJ8ENML7');
+            `,
+          }}
+        />
         {/* Organization Schema */}
         <script
           type="application/ld+json"
